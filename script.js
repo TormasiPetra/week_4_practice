b3c31f1a829ca2b7a132ccd53dc18c14051eb810
TormasiPetra <tormasi.petra@gmail.com>
2023-10-09 20:48:17 +0200

    monday_practice

--- script.js
+++ script.js
@@ -1,76 +1,89 @@
-const data = [
+const arr = ['egy', 'kettő', 'három', 'negy']
+
+// console.log(arr.includes("text"))
+// console.log(arr.includes("kettő"))
+
+console.log(["one","two", "tree", "four"].includes("four"))
+
+
+const str = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?'
+
+console.log(str.indexOf("The"))
+
+// szünet
+
+const obj = {
+    name: "Gipsz Jakab" ,
+    age: 27,
+    pets: [
+        "Mirci",
+        "Lajcsi",
+        "Géza"
+    ],
+    children: [
+        {
+        name: "Jakabb",
+        age: 5
+        }
+    ]
+}
+
+for (const key of Object.keys(obj)) {
+    console.log(key, obj[key])
+}
+
+const users = [
     {
-        name: "Kovács János",
-        age: 29,
-        subjects: [
-            {
-                subjectName: "maths",
-                subjectGrade: 5
-            },
-            {
-                subjectName: "physics",
-                subjectGrade: 4
-            },
-            {
-                subjectName: "english",
-                subjectGrade: 2
-            }
-        ]
+        name: "John Doe",
+        age: 21
     },
     {
-        name: "Csányi Mihály",
-        age: 35,
-        subjects: [
-            {
-                subjectName: "maths",
-                subjectGrade: 2
-            },
-            {
-                subjectName: "physics",
-                subjectGrade: 5
-            },
-            {
-                subjectName: "english",
-                subjectGrade: 4
-            }
-        ]
+        name: "Kovács János", 
+        age: 43
     },
     {
-        name: "Jóska Pista",
-        age: 50,
-        subjects: [
-            {
-                subjectName: "maths",
-                subjectGrade: 3
-            },
-            {
-                subjectName: "physics",
-                subjectGrade: 4
-            },
-            {
-                subjectName: "english",
-                subjectGrade: 3
-            }
-        ]
+        name: "Tormasi Petra",
+        age: 28
     }
 ]
 
-/* for ( let i = 0; i < data.length; i++){
-    console.log( data[i].name )
+users.forEach(function(user) {
+    console.log(user.name)
+})
+
+users.forEach((user) => console.log(user.name))
+
+
+
+const userNames = []
+// belepusholja a végére az elemeket
+users.forEach((user) => userNames.push(user.name))
+
+console.log(userNames)
+// ez nem adja vissza az eredményt rögtön, van ehelyett másik, ami jobb kicsit -->
+
+
+
+// a mapnek egyből van visszatérési értéke 
+// nem csak az adott elemet tudom kiírni vele, hanem az indexet is 
+const result = users.map((user, index) => console.log(index, user.name))
+
+console.log(result)
+
+
+
+const names = users.map((user, index) => {
+    return {
+        username: user.name,
+        age: user.age,
+        id: index + 1
+    }
 }
- */
+)
+console.log(names)
 
-/* data.forEach(student => console.log(student.name)) */
 
-data.forEach(student => {
-    console.log(student.name)
-    student.subjects.forEach(subject => console.log(`${subject.subjectName}: ${subject.subjectGrade}`))
+const mapped = users.map((user, index) => {
+    return `The user name is: ${user.name}, he/she is ${user.age} years old. He/she is at the ${index + 1} place.`
 })
-
-//lehetne így is:
-data.forEach(e => {
-    console.log(e.name)
-    e.subjects.forEach(e2 => console.log(`${e2.subjectName}: ${e2.subjectGrade}`))
-})
-
-
+console.log(mapped)
